5166dc1499df26d0d4aa85ece5f06f7eb9a58097
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-07-01 13:00:53 +0200

    Small update to an assert to be more explicit
    Changed the error message of the assert in the function _days_in_month in order to be more explicit month -> 'month must be in 1...12'

--- Lib/datetime.py
+++ Lib/datetime.py
@@ -50,7 +50,7 @@ def _days_before_year(year):
 
 def _days_in_month(year, month):
     "year, month -> number of days in that month in that year."
-    assert 1 <= month <= 12, month
+    assert 1 <= month <= 12, 'month must be in 1...12'
     if month == 2 and _is_leap(year):
         return 29
     return _DAYS_IN_MONTH[month]
